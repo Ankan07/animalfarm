a201e1964cd48584c34c88ed923262f46f6c7068
Ankan <ankantech96@gmail.com>
2020-03-17 01:46:11 +0530

    createcolony

--- main.py
+++ main.py
@@ -348,6 +348,74 @@ def handle_request_three():
                 })
                 return {'status': 'true'}
 
+@app.route('/v1/createcolony',methods=['POST'])
+def handle_create_colony():
+    input_params = request.get_json()
+    reqparams = ['sireId', 'breeder_ids','sire_batchId','sire_colonyId','colonyname']
+    for x in reqparams:
+        if x not in input_params:
+            return invalidUsage('Missing field: ' + x, 400)
+
+    #define the colony_object
+    colony={}
+    #get the current colony number.
+    colony_number=db.meta.find_one({"type":"colonycount"})
+    count=colony_number["count"]
+    #increase colony count
+    db.meta.update_one({"type":"colonycount"},{'$set':{'count':count+1}})
+    #generate colony name
+    colonyId='C'+str(count)
+    colonyname=input_params['colonyname']
+    #fetch selection_box_sire_details step 1
+    sire_details=db.market_selection.find_one({"_id": input_params['sireId']})
+
+    sire_batchId=input_params['sire_batchId']
+    sire_colonyId=input_params['sire_colonyId']
+
+    #creating the breeder object step 2
+    breeder_ids=input_params['breeder_ids']
+    for i in range(len(breeder_ids)):
+        breeder_object={}
+        temp=breeder_ids[i]
+        breederId=temp["breederId"]
+
+
+
+       
+        breeder_object["dames"]=temp["dames"]
+        breeder_object["ndames"]=len(breeder_object["dames"])
+        breeder_object["neonates"]=[]
+        breeder_object["_id"]=breederId
+        breeder_object["ms"]=[]
+        breeder_object["colonyId"]=colonyId
+        breeder_object["cName"]=colonyname
+   
+        breeder_object["breed"]=sire_details["breed"]
+        db.breeder.insert_one(breeder_object)
+    
+    #insering colony object
+    sire={}
+    sire["colonyId"]= sire_colonyId
+    sire["batchId"]=sire_batchId
+    colony["name"]=colonyname
+    colony["sire"]=sire
+    colony["generation"]=0
+    colony["breeders"]=[x['breederId'] for x in breeder_ids]
+    colony["ms"]=[]
+    colony["rest"]=False
+    colony["breed"]=sire_details["breed"]
+ 
+    db.colony.insert_one(colony)
+
+    return {'status':"success"}
+
+
+
+   
+    
+
+
+
 
 
 
